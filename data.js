1eefeabff4a2a7576e6394c9a8d8a494da0e5a35
Manish Kumar <121895648+ManishMandrai@users.noreply.github.com>
2025-01-03 17:36:11 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,49 +1,49 @@
-// const user = { name: "Manish", age: 24, country: "Imdia" };
+const user = { name: "Manish", age: 24, country: "Imdia" };
 
-// console.log("User Info", user);
-// console.error("This is an error message");
-// console.warn("Thus is a warning message");
-// console.table([
-//     { name: "Rohit", age: 23 },
-//     { name: "Raul", age: 25 }
-// ])
-
-
-// function calculateSum(a, b) {
-//     let sum = a + b;
-//     return sum;
-// }
-// console.log(calculateSum(45, 20));
+console.log("User Info", user);
+console.error("This is an error message");
+console.warn("Thus is a warning message");
+console.table([
+    { name: "Rohit", age: 23 },
+    { name: "Raul", age: 25 }
+])
 
 
-// function divide(a, n) {
-//     try {
-//         if (n === 0) {
-//             throw new Error("Can't divided by zero")
-//         }
-//         return a / n;
-//     } catch (error) {
-//         console.error(error.message);
-//     };
-// };
-
-// console.log(divide(10, 5));
-// console.log(divide(10, 0));
+function calculateSum(a, b) {
+    let sum = a + b;
+    return sum;
+}
+console.log(calculateSum(45, 20));
 
 
-// const product = { id: 1001, name: "Laptop", price: 23455 }
-// console.log("product details", product);
-// console.warn("stock is running low");
-// console.error("Unable to fetch stock details");
+function divide(a, n) {
+    try {
+        if (n === 0) {
+            throw new Error("Can't divided by zero")
+        }
+        return a / n;
+    } catch (error) {
+        console.error(error.message);
+    };
+};
+
+console.log(divide(10, 5));
+console.log(divide(10, 0));
 
 
-// function printNumbers(limit) {
-//     for (let i = 0; i <= limit; i++) {
-//         console.log("Number:", i);
-//     }
-// }
+const product = { id: 1001, name: "Laptop", price: 23455 }
+console.log("product details", product);
+console.warn("stock is running low");
+console.error("Unable to fetch stock details");
 
-// printNumbers(5);
+
+function printNumbers(limit) {
+    for (let i = 0; i <= limit; i++) {
+        console.log("Number:", i);
+    }
+}
+
+printNumbers(5);
 
 
 function outerFunction(){
